e1f3cd1f77f7625a2ccfb82266895876a024b0c0
abelockii80 <abelockii80@mail.ru>
2019-08-07 16:39:57 +0300

    Некорректно записывается в Эксель

--- agrobiz_parsing.py
+++ agrobiz_parsing.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 import os
+import time
 import settings
 import bs4
 import openpyxl
@@ -45,18 +46,25 @@ def id_list(html):  # Список id, чтобы потом по ним иск�
     return ids
 
 def get_email(data_id):
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, f'//div[@data-id="{data_id}"]/span')))    
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, f'//div[@data-id="{data_id}"]/span')))        
     driver.find_element_by_xpath(f'//div[@data-id="{data_id}"]/span').click()
     email = driver.find_element_by_xpath('//div[@class="contact_info_inner"]/p/a').text
     print(email)
     return email
 
 
-def write_cell(sheet_name, file, row_number, data):
+def write_collumn(sheet_name, file, email_list):
+    print(len(email_list))
     wb = openpyxl.load_workbook(file)
     sheet = wb[sheet_name]
-    sheet.cell(row=row_number, column=1,value=data)    
-    wb.save('agrobase.xlsx')
+    for email in email_list:
+        sheet.cell(row=email_list.index(email) + 1, column=1,value=email)    
+    wb.save('agrobase.xlsx')    
+    return len(email_list)
+
+
+
+
 
 
 
@@ -69,17 +77,15 @@ driver = webdriver.Firefox(executable_path=os.getcwd() + '/geckodriver', options
 auth()
 page_html = get_html(settings.URL_LIST[0]['Растениеводство'])
 list_id = id_list(page_html) # Список id по которым ищем мыло
-counter = 0
+email_list = []
 for id in list_id:
     try:
         email = get_email(id)
-        counter += 1        
-        write_cell('Растениеводство', 'agrobase.xlsx', counter, email)
-
+        email_list.append(email)            
     except NoSuchElementException:
         continue
 
-    
+write_collumn('Растениеводство', 'agrobase.xlsx', email_list)
 
 
 
